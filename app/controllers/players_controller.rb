1822bab4053fa0d23cd8cfffae8032cf3e4f2e17
flachadriano <flachadriano@gmail.com>
2015-10-17 16:22:23 -0300

    crud players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -28,7 +28,7 @@ class PlayersController < ApplicationController
 
     respond_to do |format|
       if @player.save
-        format.html { redirect_to @player, notice: 'Player was successfully created.' }
+        format.html { redirect_to players_url, notice: 'Player was successfully created.' }
         format.json { render :show, status: :created, location: @player }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class PlayersController < ApplicationController
   def update
     respond_to do |format|
       if @player.update(player_params)
-        format.html { redirect_to @player, notice: 'Player was successfully updated.' }
+        format.html { redirect_to players_url, notice: 'Player was successfully updated.' }
         format.json { render :show, status: :ok, location: @player }
       else
         format.html { render :edit }
--- db/schema.rb
+++ db/schema.rb
@@ -33,12 +33,6 @@ ActiveRecord::Schema.define(version: 20151017182733) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "provider",                            null: false
-    t.string   "uid",                                 null: false
-    t.string   "name"
-    t.string   "location"
-    t.string   "image_url"
-    t.string   "url"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "email",                  default: "", null: false
@@ -54,9 +48,6 @@ ActiveRecord::Schema.define(version: 20151017182733) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true, using: :btree
-  add_index "users", ["provider"], name: "index_users_on_provider", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["uid"], name: "index_users_on_uid", using: :btree
 
 end
