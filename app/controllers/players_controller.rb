ab3da78fc2ef3dcc7d4b79c45f473702d8ca33ba
flachadriano <flachadriano@gmail.com>
2015-10-20 02:05:06 -0200

    style into index players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,7 +4,7 @@ class PlayersController < ApplicationController
   # GET /players
   # GET /players.json
   def index
-    @players = Player.all
+    @players = Player.all.order(:session, :position_id)
   end
 
   # GET /players/1
