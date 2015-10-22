1ea34665c218a6fe9c0d3df760acd874c0537ff7
flachadriano <flachadriano@gmail.com>
2015-10-21 23:28:06 -0200

    add sell style

--- app/assets/javascripts/players.js
+++ app/assets/javascripts/players.js
@@ -2,6 +2,15 @@ function playerAtribChange(checkbox, playerId) {
 	data = {player: {}};
 	data.player[checkbox.name] = checkbox.checked;
 
+	if (checkbox.name === 'sell') {
+		var el = document.getElementById('player-' + playerId);
+		if (checkbox.checked) {
+			el.className = el.className + ' sell';
+		} else {
+			el.className = el.className.replace(' sell', '');
+		}
+	}
+
 	$.ajax({
 		type: "PUT",
 		dataType: "json",
