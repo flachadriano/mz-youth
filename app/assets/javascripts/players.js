e56654cf666de2f81ad40bddf54d3bf66b9729b3
flachadriano <flachadriano@gmail.com>
2015-11-30 22:09:45 -0200

    list class player

--- app/assets/javascripts/players.js
+++ app/assets/javascripts/players.js
@@ -1,26 +1,28 @@
 function playerAtribChange(checkbox, playerId) {
-	data = {player: {}};
-	data.player[checkbox.name] = checkbox.checked;
+  data = {
+    player: {},
+  };
+  data.player[checkbox.name] = checkbox.checked;
 
-	if (checkbox.name === 'sell') {
-		updatePlayerClass(playerId, ' sell', checkbox.checked);
-	} else if (checkbox.name === 'session_one') {
-		updatePlayerClass(playerId, ' tc', checkbox.checked);
-	}
+  if (checkbox.name === 'sell') {
+    updatePlayerClass(playerId, ' sell', checkbox.checked);
+  } else if (checkbox.name === 'session_one') {
+    updatePlayerClass(playerId, ' tc', checkbox.checked);
+  }
 
-	$.ajax({
-		type: "PUT",
-		dataType: "json",
-		url: "players/"+playerId+".json",
-		data: data
-	});
+  $.ajax({
+    type: 'PUT',
+    dataType: 'json',
+    url: 'players/' + playerId + '.json',
+    data: data,
+  });
 }
 
 function updatePlayerClass(playerId, cssClass, checked) {
-	var el = document.getElementById('player-' + playerId);
-	if (checked) {
-		el.className = el.className + cssClass;
-	} else {
-		el.className = el.className.replace(cssClass, '');
-	}
+  var el = document.getElementById('player-' + playerId);
+  if (checked) {
+    el.className = el.className + cssClass;
+  } else {
+    el.className = el.className.replace(cssClass, '');
+  }
 }
